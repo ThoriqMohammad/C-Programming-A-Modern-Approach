33ec6fc400559e3637e35adac66de539d2f6915f
Thoriq Mohammad <thoriqmohammad89@gmail.com>
2026-01-17 14:08:40 +0700

    Folder reorganizationing

--- Chapter-02/Programming Project 2-1/ProgrammingProject2-1.c
+++ Chapter-02/Programming Project 2-1/ProgrammingProject2-1.c
@@ -1,1 +1,22 @@
+/*Write a program that uses printf to display the following picture on the screen:
+            *
+           *
+          *
+   *     *
+    *   *
+      *
+*/
 
+/*Answer */
+
+#include <stdio.h>
+int main()
+{
+    printf("       *\n");
+    printf("      * \n");
+    printf("     *  \n");
+    printf("*   *   \n");
+    printf(" * *    \n");
+    printf("  *     \n");
+    return 0;
+}
