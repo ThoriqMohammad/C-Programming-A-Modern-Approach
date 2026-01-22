5735f865e5ff36953fea7af1ee9deca9bfc1d50d
Thoriq Mohammad <thoriqmohammad89@gmail.com>
2026-01-22 08:18:11 +0700

    Modify polynomial evaluation to use Horner's Rule
    Implement polynomial evaluation using Horner's Rule.

--- Chapter-02/Programming Project 2-6/ProgrammingProject2-6.c
+++ Chapter-02/Programming Project 2-6/ProgrammingProject2-6.c
@@ -1,1 +1,26 @@
+/* Problem : 
+* Modify the program of Programming Project 5 so that the polynomial is evaluated using the
+* following formula:
+* ((((3x + 2)x – 5)x – 1)x + 7)x – 6
+* Note that the modified program performs fewer multiplications. This technique for evaluat-
+* ing polynomials is known as Horner’s Rule.
+*/
 
+/* Solution */
+
+#include <stdio.h>
+int main() 
+{
+    float x, y1, y2, y3, y4, y5;
+    printf("Enter a value: ");
+    scanf("%f", &x);
+    y1 = 3.0f * x + 2.0;
+    y2 = y1 * x - 5.0f;
+    y3 = y2 * x - 1.0f;
+    y4 = y3 * x + 7.0f;
+    y5 = y4 * x - 6.0f;
+    
+    printf("%f\n", y5);
+
+  return 0;
+}
