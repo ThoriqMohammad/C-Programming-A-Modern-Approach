ff562ec4a9ee6427c3c3a0cc5d1dc7c51ef54cba
Thoriq Mohammad <thoriqmohammad89@gmail.com>
2026-02-13 13:50:58 +0700

    Update broker program to include rival commission
    Modified the broker program to ask for the number of shares and price per share. Added rival broker commission calculation based on share count.

---
+++ Chapter-05/Programming Project 5-3/ProgrammingProject5-3.c
@@ -0,0 +1,74 @@
+/* Problem 
+* Modify the broker.c program of Section 5.2 by making both of the following changes:
+* (a) Ask the user to enter the number of shares and the price per share, instead of the value
+* of the trade.
+* (b) Add statements that compute the commission charged by a rival broker ($33 plus 3¢ per
+* share for fewer than 2000 shares; $33 plus 2¢ per share for 2000 shares or more). Dis-
+* play the rival’s commission as well as the commission charged by the original broker.
+*/
+
+/* Solution */
+
+/* Calculates a broker's commission */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float commission, value, price_per_share, commission_rival;
+    int num_share;
+    
+    printf("Enter the number of shares: ");
+    scanf("%d", &num_share);
+    
+    printf("Enter the price per share: ");
+    scanf("%f", &price_per_share);
+    
+    value = num_share * price_per_share;
+    
+    if (value < 2500.00f)
+    {
+        commission = 30.00f + .017f * value;
+    }
+    else if (value < 6250.00f)
+    {
+        commission = 56.00f + .0066f * value;
+    }
+    else if (value < 20000.00f)
+    {
+        commission = 76.00f + .0034f * value;
+    }
+    else if (value < 50000.00f)
+    {
+        commission = 100.00f + .0022f * value;
+    }
+    else if (value < 500000.00f)
+    {
+        commission = 155.00f + .0011f * value;
+    }
+    else
+    {
+        commission = 255.00f + .0009f * value;
+    }
+    
+    if (commission < 39.00f)
+    {
+        commission = 39.00f;
+    }
+    
+    printf("Original broker's commission: $%.2f\n", commission);
+    
+    if (num_share < 2000)
+    {
+        commission_rival = 33.00f + 0.03f * num_share;
+    }
+    else
+    {
+        commission_rival = 33.00f + 0.02f * num_share;
+    }
+    
+    printf("Rival broker's commission: $%.2f\n", commission_rival);
+    
+    return 0;
+
+}
