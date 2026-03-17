640403374a88a022d5b6631bdbca925214a52181
Thoriq Mohammad <thoriqmohammad89@gmail.com>
2026-03-17 16:26:00 +0700

    Implement pause feature after every 24 squares
    Added functionality to pause after every 24 squares and prompt the user to press Enter to continue.

---
+++ Chapter-07/Programming Project 7-2/ProgrammingProject7-2.c
@@ -0,0 +1,38 @@
+/* Problem
+* Modify the square2.c program of Section 6.3 so that it pauses after every 24 squares and
+* displays the following message:
+* Press Enter to continue...
+* After displaying the message, the program should use getchar to read a character.
+* getchar won’t allow the program to continue until the user presses the Enter key.
+*/
+
+/* Solution */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, n;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    /* Clear the input buffer to remove the newline left by scanf */
+    while (getchar() != '\n')
+        ;
+
+    for (i = 1; i <= n; i++) {
+        printf("%10d%10d\n", i, i * i);
+
+        /* Pause after every 24 squares */
+        if (i % 24 == 0) {
+            printf("Press Enter to continue...\n");
+            /* Wait for the user to press Enter */
+            while (getchar() != '\n')
+                ;
+        }
+    }
+
+    return 0;
+}
