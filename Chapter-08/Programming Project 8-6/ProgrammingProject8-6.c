d7d31cbb553e9bfb3e26e524524850e6ddaae280
Thoriq Mohammad <thoriqmohammad89@gmail.com>
2026-04-25 08:46:24 +0700

    Add B1FF filter program
    Implement B1FF filter to translate messages into B1FF-speak.

---
+++ Chapter-08/Programming Project 8-6/ProgrammingProject8-6.c
@@ -0,0 +1,43 @@
+/* Problem 
+* The prototypical Internet newbie is a fellow named B1FF, who has a unique way of writing
+* messages. Here’s a typical B1FF communiqué:
+* H3Y DUD3, C 15 R1LLY C00L!!!!!!!!!!
+* Write a “B1FF filter” that reads a message entered by the user and translates it into B1FF-
+* speak:
+* Enter message: Hey dude, C is rilly cool
+* In B1FF-speak: H3Y DUD3, C 15 R1LLY C00L!!!!!!!!!!
+* Your program should convert the message to upper-case letters, substitute digits for certain
+* letters (A→4, B→8, E→3, I→1, O→0, S→5), and then append 10 or so exclamation marks.
+* Hint: Store the original message in an array of characters, then go back through the array,
+* translating and printing characters one by one.
+*/
+
+/* Solution */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    int i;
+    
+    char ch[25];
+    printf("Enter message:");
+    for(i = 0; i < 25; i++){
+        scanf("%ch", &ch[i]);
+    }
+    
+    for(i = 0; i < 25; i++){
+        switch(toupper(ch[i])) {
+            case 'A': printf("4"); break;
+            case 'O': printf("0"); break;
+            case 'B': printf("8"); break;
+            case 'E': printf("3"); break;
+            case 'I': printf("1"); break;
+            case 'S': printf("5"); break;
+            default : printf("%c", toupper(ch[i])); break;
+        }
+    }
+    printf("!!!!!!!!!");
+
+    return 0;
+}
